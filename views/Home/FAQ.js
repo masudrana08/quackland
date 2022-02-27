cee2fc925f021ad2c2bb8624e1c339ab56a3ea8f
masudx08 <promasud225@gmail.com>
2022-02-27 20:42:18 +0600

    faq done

--- views/Home/FAQ.js
+++ views/Home/FAQ.js
@@ -6,32 +6,34 @@ const FAQ = () => {
   return (
     <div className='faqC'>
       <h2>Frequently Asked Question</h2>
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>How to join with the team?</Accordion.Header>
-          <Accordion.Body>
+      <div className="inner">
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>How to join with the team?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>What benefits will the Community have?</Accordion.Header>
+            <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>What benefits will the Community have?</Accordion.Header>
-          <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>In what way will the Community Wallet Funds be used?</Accordion.Header>
-          <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="3">
-          <Accordion.Header>Where can I connect with the community?</Accordion.Header>
-          <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>In what way will the Community Wallet Funds be used?</Accordion.Header>
+            <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Where can I connect with the community?</Accordion.Header>
+            <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin pharetra ultrices.Fusce elit libero, venenatis vel consequat in,
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
     </div>
   );
 };
