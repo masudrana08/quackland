c650e4a7bda90676dd7d91300fc977b6be6234c3
masudrana08 <rana028511@gmail.com>
2023-02-01 18:43:37 +0600

    problem fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css'
 import '../styles/mynavbar.css'
@@ -8,9 +7,21 @@ import '../styles/roadmap.css'
 import '../styles/faq.css'
 import '../styles/team.css'
 import '../styles/footer.css'
+import React, { Component, FunctionComponent, ReactElement } from 'react';
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
+type AppProps = {
+  Component: any,
+  pageProps: any
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
